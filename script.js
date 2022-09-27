80006844cdc86611b402844fc2c5bb3244395b01
Eduardo <eduardo.eduesf@gmail.com>
2022-09-27 20:34:58 -0300

    Juastando os Exes

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const frutas = ["laranja", "limão", "uva"];
 
 let fruta1 = document.getElementById("fruta-1");
-fruta1.innerHTML = frutas[0];
-
 let fruta2 = document.getElementById("fruta-2");
-fruta2.innerHTML = frutas[1];
-
 let fruta3 = document.getElementById("fruta-3");
-fruta3.innerHTML = frutas[2];
-
 let fruta4 = document.getElementById("fruta-4");
+
+fruta1.innerHTML = frutas[0];
+fruta2.innerHTML = frutas[1];
+fruta3.innerHTML = frutas[2];
 
 function novoInput() {
   console.log(nomeDaFruta.value);
@@ -17,4 +15,10 @@ function novoInput() {
 
 function novaFruta() {
   fruta4.innerHTML = nomeDaFruta.value;
+  nomeDaFruta.value = "";
 }
+
+// // fazendo o EXE 1 com For
+// for (let i = 0; i < frutas.length; i++) {
+//   document.getElementById(`fruta-${[i + 1]}`).innerHTML = frutas[i];
+// }
