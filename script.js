c369532c0a6c7fa71ba2f9bf72e547cd6a44277f
Hugowave88 <hugohedgehog24@gmail.com>
2022-09-27 19:51:23 -0300

    DOM-I

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 const frutas = ["laranja", "limão", "uva"];
+
+let fruta1= document.getElementById("fruta-1");
+fruta1.innerHTML =frutas[0]
+
+let fruta2= document.getElementById("fruta-2");
+fruta2.innerHTML =frutas[1]
+
+let fruta3= document.getElementById("fruta-3");
+fruta3.innerHTML =frutas[2]
+
+let fruta4= document.getElementById("fruta-4");
+
+let input= document.getElementById("name");
+
+function imprimir(){
+    console.log(input.value)
+
+}
+
+function adicionar(){
+fruta4.innerHTML=input.value
+
+}
+dwq-dpfv-eoz
