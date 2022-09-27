5c73a220bed78029f823596b63b9f112bdbf2fce
Everton Mello <everton.mello.281@gmail.com>
2022-09-26 22:03:34 -0300

    Exercícios aula Dom I ok

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 const frutas = ["laranja", "limão", "uva"];
+
+const fruta1 = document.getElementById('fruta-1');
+fruta1.innerHTML = frutas[0]
+
+const fruta2 = document.getElementById('fruta-2');
+fruta2.innerHTML = frutas[1]
+
+const fruta3 = document.getElementById('fruta-3');
+fruta3.innerHTML = frutas[2]
+
+const frutaImput = document.getElementById("inputFruta")
+console.log(frutaImput.value)
+
+const fruta4 = document.getElementById("fruta-4")
+
+function adicionaFruta() {
+    fruta4.innerHTML = frutaImput.value
+}
