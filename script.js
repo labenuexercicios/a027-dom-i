6c340e61f9b4ce5fce7503af229140e69ef6d8ae
Everton Mello <everton.mello.281@gmail.com>
2022-09-27 20:24:04 -0300

    Codigo melhorado!

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const frutas = ["laranja", "limão", "uva"];
 
-const fruta1 = document.getElementById('fruta-1');
-fruta1.innerHTML = frutas[0]
+for(let i = 0; i < frutas.length; i++){
+    document.getElementById(`fruta-${i+1}`).innerHTML = frutas[i]
+}
 
-const fruta2 = document.getElementById('fruta-2');
-fruta2.innerHTML = frutas[1]
+// const fruta1 = document.getElementById('fruta-1');
+// fruta1.innerHTML = frutas[0]
 
-const fruta3 = document.getElementById('fruta-3');
-fruta3.innerHTML = frutas[2]
+// const fruta2 = document.getElementById('fruta-2');
+// fruta2.innerHTML = frutas[1]
+
+// const fruta3 = document.getElementById('fruta-3');
+// fruta3.innerHTML = frutas[2]
 
 const frutaImput = document.getElementById("inputFruta")
 console.log(frutaImput.value)
@@ -17,3 +21,4 @@ const fruta4 = document.getElementById("fruta-4")
 function adicionaFruta() {
     fruta4.innerHTML = frutaImput.value
 }
+
