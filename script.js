64a20e88129b7521eb0cdaca9eea79a465f07dce
Daniel <daniel.sprcp@gmail.com>
2022-09-27 18:11:45 -0300

    Exercício 1 e 2 finalizados

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 const frutas = ["laranja", "limão", "uva"];
+
+const adicionaFruta1 = document.getElementById("fruta-1") 
+adicionaFruta1.innerHTML = frutas[0]
+
+const adicionaFruta2 = document.getElementById("fruta-2") 
+adicionaFruta2.innerHTML = frutas[1]
+
+const adicionaFruta3 = document.getElementById("fruta-3") 
+adicionaFruta3.innerHTML = frutas[2]
+
