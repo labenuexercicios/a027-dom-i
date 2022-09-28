3e4847f1de944e142f378c429a96d5f4314cb0ed
Jaziel <jaziorcc@hotmail.com>
2022-09-27 21:09:04 -0300

    Exercicio a027

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 const frutas = ["laranja", "limão", "uva"];
+
+const frutaUm = document.getElementById("fruta-1")
+
+frutaUm.innerHTML= frutas[0]
+
+const frutaDois = document.getElementById("fruta-2")
+frutaDois.innerHTML = frutas[1]
+
+const frutaTre = document.getElementById("fruta-3")
+frutaTre.innerHTML = frutas[2]
+
+const imp = document.getElementById("name")
+console.log(imp.value)
+
+
+function clic(){
+    console.log('ola')
+}
+
+const clicDois = () => {
+    const frutaQua = document.getElementById("fruta-4")
+    frutaQua.innerHTML = imp.value
+}
+
+
+
+
