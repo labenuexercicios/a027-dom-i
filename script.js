eee2add02d2f327e7147419b08235e77bfa6b432
GLCalegaro <giovaluiza@gmail.com>
2022-10-03 16:05:09 -0300

    Exercicios aula 27 resolvidos

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 const frutas = ["laranja", "limão", "uva"];
+const listaFrutas = document.getElementById`lista-de-frutas`;
+
+const fruta1 = document.getElementById("Fruta 1");
+
+const fruta2 = document.getElementById("Fruta 2");
+
+const fruta3 = document.getElementById("Fruta 3");
+
+const fruta4 = document.getElementById("fruta-4");
+
+for (let i = 0; i <= frutas.length; i++) {
+    document.getElementById(`fruta-${[i + 1]}`).innerHTML = frutas[i]
+}
+
+const textFruta = document.getElementById(`frutaText`);
+function addFruta () {
+    frutas.push(textFruta.value);
+    console.log(frutas);
+}    
