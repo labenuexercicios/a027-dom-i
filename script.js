d76250218056fd95de658fc09f8663a93b2e5527
Polyana-Rangel <polyanarangel17@gmail.com>
2022-10-03 16:02:45 -0300

    DOM

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const frutas = ["laranja", "limão", "uva"];
+const frutas = ["laranja", "limão", "uva", "maça"];
+
+
+
+let listaDeFrutas = document.getElementsByTagName ("li")
+
+for(let i in frutas){
+    listaDeFrutas[i].innerHTML=frutas[i]
+   
+}
+
+const adicionaItem = () => {
+    const item = document.getElementById("texto")
+    const lista = document.getElementById("lista-de-frutas")
+    lista.innerHTML += `<li>${item.value}</li>`
+    item.value = ""
+}
