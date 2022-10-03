84d6eb07b06a8087d5b242a009b21dc732e71d46
guirozmesquita@gmail.com <guilhermesquita>
2022-10-03 11:56:55 -0400

    exercicio ok

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 const frutas = ["laranja", "limão", "uva"];
+
+const fruta1 = document.getElementById('fruta-1')
+fruta1.innerHTML = frutas[0]
+
+
+const fruta2 = document.getElementById('fruta-2')
+fruta2.innerHTML = frutas[1]
+
+
+const fruta3 = document.getElementById('fruta-3')
+fruta3.innerHTML = frutas[2]
+
+const fruta4 = document.getElementById('fruta-4')
+fruta4.innerHTML = frutas[3]
+
+
+novaFruta = document.getElementById('fruta')
+
+function digitar() {
+    console.log(novaFruta.value);
+}
+
+function adicionar() {
+    fruta4.innerHTML = novaFruta.value
+}
