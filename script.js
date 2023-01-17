26bbb977d292238327cff3c3e8f2b77d0fa763e8
Rlopesn <nogueirarln@gmail.com>
2023-01-17 20:15:17 -0300

    exercicio DOM 1

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 const frutas = ["laranja", "limão", "uva"];
+
+let mostrarFruta = document.getElementById("fruta-1")
+mostrarFruta.innerHTML = frutas[0]
+
+mostrarFruta = document.getElementById("fruta-2")
+mostrarFruta.innerHTML = frutas[1]
+
+mostrarFruta = document.getElementById("fruta-3")
+mostrarFruta.innerHTML = frutas[2]
+
+const fruta4 = document.getElementById("fruta-4")
+let quartaFruta = document.getElementById("fruta")
+
+
+
+
+const adicionarFruta = () => {
+   fruta4.innerHTML = quartaFruta.value
+   
+   quartaFruta.value = ""
+    
+}
+
+const colocarFruta = () => {
+    adicionarFruta()
+}
