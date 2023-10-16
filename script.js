33ea4cb74c6bf9681e9af12c9dcd26b61cee3869
raissanfg <raissanfg@gmail.com>
2023-10-16 13:42:08 -0300

    exercicio-1-done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+
+//exercicio 1 
+
 const frutas = ["laranja", "limão", "uva"]
+
+const listaDeFrutas =  document.getElementById("fruta-1")
+
+console.log(listaDeFrutas.innerHTML)
+listaDeFrutas.innerHTML += frutas[0];
+
+const frutasDois =  document.getElementById("fruta-2")
+frutasDois.innerHTML += frutas[1];
+
+const frutasTres =  document.getElementById("fruta-3")
+frutasTres.innerHTML += frutas[2]
+
+
